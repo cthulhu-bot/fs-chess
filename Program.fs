b2571b0ae9186ab6f35a0763cf75a058781ad32b
Joshua Stupplebeen <jpstup@gmail.com>
2016-10-21 13:58:25 -0600

    initial pass on visualization working

--- Program.fs
+++ Program.fs
@@ -2,99 +2,129 @@
 
 open System
 
-// Treat the board like state and have actions reduce the existing board state and the action resulting in a new board state
-let state = initialBoard
+// Board initialization
+module Board =
+    type coord = {Coordinate:string; Piece:string}
 
-let initialBoard =
-    Map.empty.
-        Add("A1", "wc1").
-        Add("A2", "wp1").
-        Add("A3", "").
-        Add("A4", "").
-        Add("A5", "").
-        Add("A6", "").
-        Add("A7", "bp1").
-        Add("A8", "bc1").
-        Add("B1", "wk1").
-        Add("B2", "wp2").
-        Add("B3", "").
-        Add("B4", "").
-        Add("B5", "").
-        Add("B6", "").
-        Add("B7", "bp2").
-        Add("B8", "bk1").
-        Add("C1", "wb1").
-        Add("C2", "wp3").
-        Add("C3", "").
-        Add("C4", "").
-        Add("C5", "").
-        Add("C6", "").
-        Add("C7", "bp3").
-        Add("C8", "bb1").
-        Add("D1", "wQ").
-        Add("D2", "wp4").
-        Add("D3", "").
-        Add("D4", "").
-        Add("D5", "").
-        Add("D6", "").
-        Add("D7", "bp4").
-        Add("D8", "bQ").
-        Add("E1", "wK").
-        Add("E2", "wp5").
-        Add("E3", "").
-        Add("E4", "").
-        Add("E5", "").
-        Add("E6", "").
-        Add("E7", "bp5").
-        Add("E8", "bK").
-        Add("F1", "wb2").
-        Add("F2", "wp6").
-        Add("F3", "").
-        Add("F4", "").
-        Add("F5", "").
-        Add("F6", "").
-        Add("F7", "bp6").
-        Add("F8", "bb2").
-        Add("G1", "wk2").
-        Add("G2", "wp7").
-        Add("G3", "").
-        Add("G4", "").
-        Add("G5", "").
-        Add("G6", "").
-        Add("G7", "bp7").
-        Add("G8", "bk2").
-        Add("H1", "wc2").
-        Add("H2", "wp8").
-        Add("H3", "").
-        Add("H4", "").
-        Add("H5", "").
-        Add("H6", "").
-        Add("H7", "bp8").
-        Add("H8", "bc2")
+    let intialBoard = [
+            {Coordinate="A1"; Piece="wc1"}
+            {Coordinate="A2"; Piece="wp1"}
+            {Coordinate="A3"; Piece=""}
+            {Coordinate="A4"; Piece=""}
+            {Coordinate="A5"; Piece=""}
+            {Coordinate="A6"; Piece=""}
+            {Coordinate="A7"; Piece="bp1"}
+            {Coordinate="A8"; Piece="bc1"}
+            {Coordinate="B1"; Piece="wk1"}
+            {Coordinate="B2"; Piece="wp2"}
+            {Coordinate="B3"; Piece=""}
+            {Coordinate="B4"; Piece=""}
+            {Coordinate="B5"; Piece=""}
+            {Coordinate="B6"; Piece=""}
+            {Coordinate="B7"; Piece="bp2"}
+            {Coordinate="B8"; Piece="bk1"}
+            {Coordinate="C1"; Piece="wb1"}
+            {Coordinate="C2"; Piece="wp3"}
+            {Coordinate="C3"; Piece=""}
+            {Coordinate="C4"; Piece=""}
+            {Coordinate="C5"; Piece=""}
+            {Coordinate="C6"; Piece=""}
+            {Coordinate="C7"; Piece="bp3"}
+            {Coordinate="C8"; Piece="bb1"}
+            {Coordinate="D1"; Piece="wQ"}
+            {Coordinate="D2"; Piece="wp4"}
+            {Coordinate="D3"; Piece=""}
+            {Coordinate="D4"; Piece=""}
+            {Coordinate="D5"; Piece=""}
+            {Coordinate="D6"; Piece=""}
+            {Coordinate="D7"; Piece="bp4"}
+            {Coordinate="D8"; Piece="bQ"}
+            {Coordinate="E1"; Piece="wK"}
+            {Coordinate="E2"; Piece="wp5"}
+            {Coordinate="E3"; Piece=""}
+            {Coordinate="E4"; Piece=""}
+            {Coordinate="E5"; Piece=""}
+            {Coordinate="E6"; Piece=""}
+            {Coordinate="E7"; Piece="bp5"}
+            {Coordinate="E8"; Piece="bK"}
+            {Coordinate="F1"; Piece="wb2"}
+            {Coordinate="F2"; Piece="wp6"}
+            {Coordinate="F3"; Piece=""}
+            {Coordinate="F4"; Piece=""}
+            {Coordinate="F5"; Piece=""}
+            {Coordinate="F6"; Piece=""}
+            {Coordinate="F7"; Piece="bp6"}
+            {Coordinate="F8"; Piece="bb2"}
+            {Coordinate="G1"; Piece="wk2"}
+            {Coordinate="G2"; Piece="wp7"}
+            {Coordinate="G3"; Piece=""}
+            {Coordinate="G4"; Piece=""}
+            {Coordinate="G5"; Piece=""}
+            {Coordinate="G6"; Piece=""}
+            {Coordinate="G7"; Piece="bp7"}
+            {Coordinate="G8"; Piece="bk2"}
+            {Coordinate="H1"; Piece="wc2"}
+            {Coordinate="H2"; Piece="wp8"}
+            {Coordinate="H3"; Piece=""}
+            {Coordinate="H4"; Piece=""}
+            {Coordinate="H5"; Piece=""}
+            {Coordinate="H6"; Piece=""}
+            {Coordinate="H7"; Piece="bp8"}
+            {Coordinate="H8"; Piece="bc2"}
+    ]
 
-let visualizeBoard board =
-    let line = "_________________"
-    let columns = "| | | | | | | | |"
 
-    board |> List.map
+// Movement code
+module Moves =
+    type move = {Coord1:String; Coord2:String}
 
-    let column_A = []
 
-    let whiteStarterBackRow = "|c|k|b|Q|K|b|k|c|"
-    let blackStarterBackRow = "|c|k|b|Q|K|b|k|c|"
-    let pawnStarterRow = "|p|p|p|p|p|p|p|p|"
+// Board visualization code
+module Visualization =
+    let visualizeBoard board =
+        let line = "_________________"
+        let columns = "| | | | | | | | |"
 
-    [line;whiteStarterBackRow;line;pawnStarterRow;line;columns;line;columns;line;pawnStarterRow;line;blackStarterBackRow;line]
+        let boardViz board =
+            let delimiters = List.map (fun (coord:Board.coord) -> 
+                                match coord.Piece with
+                                | "" -> "| |"
+                                | _ -> String.concat "" ["|"; coord.Piece; "|"]) board
+            let boardSeq = seq { for a in delimiters do yield a }
+            for i = 0 to 7 do
+                let slice = Seq.skip (8 * i) boardSeq 
+                let column = Seq.take 8 slice
+                printfn "%A" (String.concat "" column)
+                printfn "\n"
+            boardSeq
 
-let printBoard board =
-    board |> List.map (printfn "%A")
 
-[<EntryPoint>]
-let main argv = 
-    // pfft for loops
-    // for KeyValue(k,v) in initialBoard do
-    //     printfn "board coord is: %s, piece present is %s" k v
-    initialBoard |> Map.iter (fun key value ->
-        printfn "Key: %A, Val: %A" key value)
-    0 // return an integer exit code
+        let whiteStarterBackRow = "|c|k|b|Q|K|b|k|c|"
+        let blackStarterBackRow = "|c|k|b|Q|K|b|k|c|"
+        let pawnStarterRow = "|p|p|p|p|p|p|p|p|"
+
+        boardViz board
+
+
+
+// Game code
+module Game =
+    let initialBoard = Board.intialBoard
+
+    let printBoard board =
+        initialBoard |> List.iter (fun coord ->
+            printfn "Key: %A, Val: %A" coord.Coordinate coord.Piece)
+
+    let movePiece board piece coord =
+        printBoard board
+    
+
+
+    [<EntryPoint>]
+    let main argv = 
+        // pfft for loops
+        // for KeyValue(k,v) in initialBoard do
+        //     printfn "board coord is: %s, piece present is %s" k v
+        printfn "%A" (Visualization.visualizeBoard initialBoard)
+        0 // return an integer exit code
 
